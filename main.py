a23f19a52c0622403fbc21adb5cbff1e14168628
V. Quintella <vitor.quintella@gmail.com>
2021-11-26 18:34:57 -0300

    Update main.py

--- main.py
+++ main.py
@@ -9,23 +9,23 @@ app = Flask(__name__)
 api = Api(app)
 
 
-class Users(Resource):
+class Dados(Resource):
     def get(self):
+        df = self._get_data()
+        df = df.to_dict()  # convert dataframe to dictionary
+        return {'data': df}, 10  # return data and 200 OK code
+
+    def _get_data(self):
         url = 'https://raw.githubusercontent.com/vitorquintella/API_exercicio/main/get_from_web/BankChurners.csv'
-        data = pd.read_csv(url,delimiter=";")
-        # data = pd.read_csv('users.csv')  # read CSV
-        data = data.to_dict()  # convert dataframe to dictionary
-        return {'data': data}, 10  # return data and 200 OK code
+        return pd.read_csv(url, delimiter=";")
 
+class Graficos(Resource):
+    def get(self):
+        return "Em construção"
 
+api.add_resource(Dados, '/dados')  # '/dados' is our entry point for dados
+api.add_resource(Graficos, '/graficos')  # '/dados' is our entry point for dados
 
-class Locations(Resource):
-    # methods go here
-    pass
-
-
-api.add_resource(Users, '/users')  # '/users' is our entry point for Users
-api.add_resource(Locations, '/locations')  # and '/locations' is our entry point for Locations
 
 if __name__ == '__main__':
     app.run()  # run our Flask app
