504c4ff5daa3ccb678a93105fbb16f62a72124bb
V.Quintella <vitor.quintella@gmail.com>
2021-11-27 21:50:12 -0300

    Finalizada atividade básica

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
 from flask import Flask
-from flask_restful import Resource, Api, reqparse
+from flask_restful import Resource, Api
 import pandas as pd
-import json
-import requests
-import ast
+import matplotlib.pyplot as plt
 
 app = Flask(__name__)
 api = Api(app)
@@ -19,12 +17,27 @@ class Dados(Resource):
     def __init__(self):
         super().__init__()
         self.df = fetch_exercise_data_online()
+        self.df_groupedby = None
 
     def get(self, card):
         df = self._filter_by_card(card)
-        df = df.head(10)
-        print(df)
-        return df.to_json()
+        self._agrupar_marital_status(df)
+        self._perpetuar_agrupados()
+        self._generate_plot()
+
+        return self.df_groupedby.to_json()
+
+    def _perpetuar_agrupados(self):
+        self.df_groupedby.to_csv('grouped_data.csv')
+
+    def _agrupar_marital_status(self, df):
+        self.df_groupedby = df.groupby('Marital_Status').mean()[['Credit_Limit']]
+
+    def _generate_plot(self):
+        plt.bar(x=self.df_groupedby.reset_index()['Marital_Status'],
+                height=self.df_groupedby.reset_index()['Credit_Limit'])
+        plt.title("Plot generated using Matplotlib")
+        plt.savefig("Grouped_plot.png")
 
     def _filter_by_card(self, card):
         dado_procurado = self.df['Card_Category'] == card
@@ -32,12 +45,8 @@ class Dados(Resource):
         return df
 
 
-class Graficos(Resource):
-    def get(self):
-        return "Em construção"
-
 api.add_resource(Dados,  '/<string:card>')  # '/dados' is our entry point for dados
-api.add_resource(Graficos, '/graficos')  # '/dados' is our entry point for dados
+# http://127.0.0.1:5000/Blue
 
 
 if __name__ == '__main__':
