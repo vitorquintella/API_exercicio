a9180763ff85f09917fa98a56dfbb973dc1b2d05
V. Quintella <vitor.quintella@gmail.com>
2021-11-29 21:58:28 -0300

    Upgrade de graficos

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from flask import Flask
 from flask_restful import Resource, Api
 import pandas as pd
 import matplotlib.pyplot as plt
+import seaborn as sns
+
 
 app = Flask(__name__)
 api = Api(app)
@@ -12,32 +14,57 @@ def fetch_exercise_data_online():
     return pd.read_csv(url)
 
 
-class Dados(Resource):
+class Filtro(Resource):
     # https://towardsdatascience.com/the-right-way-to-build-an-api-with-python-cd08ab285f8f
     def __init__(self):
         super().__init__()
         self.df = fetch_exercise_data_online()
+        self.df = self.df.drop(columns=[
+            'Naive_Bayes_Classifier_Attrition_Flag_Card_Category_Contacts_Count_12_mon_Dependent_count_Education_Level_Months_Inactive_12_mon_1',
+            'Naive_Bayes_Classifier_Attrition_Flag_Card_Category_Contacts_Count_12_mon_Dependent_count_Education_Level_Months_Inactive_12_mon_2'
+        ])
         self.df_groupedby = None
+        print('Pode-se testar com: http://127.0.0.1:5000/Blue')
 
     def get(self, card):
-        df = self._filter_by_card(card)
+        self.card = card
+        df = self._filter_by_card(self.card)
         self._agrupar_marital_status(df)
         self._perpetuar_agrupados()
-        self._generate_plot()
+        self._generate_grouped_plain_plot()
+        self._generate_Churn_Education_plot()
+        self._generate_corr_plot()
 
         return self.df_groupedby.to_json()
 
     def _perpetuar_agrupados(self):
-        self.df_groupedby.to_csv('grouped_data.csv')
+        self.df_groupedby.to_csv(f'{self.card}_card_GroupedData.csv')
 
     def _agrupar_marital_status(self, df):
-        self.df_groupedby = df.groupby('Marital_Status').mean()[['Credit_Limit']]
+        self.df_groupedby = df.groupby('Marital_Status').mean()[['Total_Trans_Amt']]
 
-    def _generate_plot(self):
-        plt.bar(x=self.df_groupedby.reset_index()['Marital_Status'],
-                height=self.df_groupedby.reset_index()['Credit_Limit'])
-        plt.title("Plot generated using Matplotlib")
-        plt.savefig("Grouped_plot.png")
+    def _generate_grouped_plain_plot(self):
+        plt.figure(figsize=(10, 5))
+        plot1 = plt.bar(x=self.df_groupedby.reset_index()['Marital_Status'],
+                height=self.df_groupedby.reset_index()['Total_Trans_Amt'])
+        plt.title(f'{self.card} card: Gasto agrupado por Total_Trans_Amt')
+        plt.savefig(f'{self.card}_card_SimplesGastoAgrupadoPorMaritalStatus.png')
+        plt.clf()
+
+    def _generate_Churn_Education_plot(self):
+        plt.figure(figsize=(10, 5))
+        plot2 = sns.countplot(x=self.df['Education_Level'], hue=self.df['Attrition_Flag'])
+        plt.title(f'{self.card} card: Churn por Education Level')
+        plt.savefig(f'{self.card}_card_ChurnporEducationLevel.png')
+        plt.clf()
+
+    def _generate_corr_plot(self):
+        plt.figure(figsize=(10, 8))
+        plot3 = sns.heatmap(self.df.corr(), cmap='coolwarm', vmin=-1, vmax=1)
+        plt.title(f'{self.card} card: HeatMap de correlação')
+        plt.savefig(f'{self.card}_card_HeatMapDeCorrelacao.png')
+        plt.clf()
+
 
     def _filter_by_card(self, card):
         dado_procurado = self.df['Card_Category'] == card
@@ -45,8 +72,7 @@ class Dados(Resource):
         return df
 
 
-api.add_resource(Dados,  '/<string:card>')  # '/dados' is our entry point for dados
-# http://127.0.0.1:5000/Blue
+api.add_resource(Filtro,  '/<string:card>')  # '/dados' is our entry point for dados
 
 
 if __name__ == '__main__':
