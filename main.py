8456fbf10241b9831758af170480e45ca3b82b98
V. Quintella <vitor.quintella@gmail.com>
2021-11-29 22:53:00 -0300

    Major: Adicionado atributo df filtrado
     - Adicionado atributo df filtrado self.df_card para garantir que a cada get, os filtros sao reaplicados sobre a base original.
    - Refactoring e limpeza do código

--- main.py
+++ main.py
@@ -23,15 +23,16 @@ class Filtro(Resource):
             'Naive_Bayes_Classifier_Attrition_Flag_Card_Category_Contacts_Count_12_mon_Dependent_count_Education_Level_Months_Inactive_12_mon_1',
             'Naive_Bayes_Classifier_Attrition_Flag_Card_Category_Contacts_Count_12_mon_Dependent_count_Education_Level_Months_Inactive_12_mon_2'
         ])
+        self.df_card = None
         self.df_groupedby = None
 
     def get(self, card):
         self.card = card
-        df = self._filter_by_card(self.card)
-        self._agrupar_marital_status(df)
+        self.df_card = self._filter_by_card(self.card)
+        self.df_groupedby = self._agrupar_marital_status()
         self._perpetuar_agrupados()
         self._generate_grouped_plain_plot()
-        self._generate_Churn_Education_plot()
+        self._generate_churn_education_plot()
         self._generate_corr_plot()
 
         return f'Tudo ok: Dados do cartao "{self.card}" persistidos no sistema.'
@@ -40,36 +41,36 @@ class Filtro(Resource):
         self.df_groupedby.to_csv(f'{self.card}_card_GroupedData.csv')
         self.df_groupedby.to_json(f'{self.card}_card_GroupedData.json')
 
-    def _agrupar_marital_status(self, df):
-        self.df_groupedby = df.groupby('Marital_Status').mean()[['Total_Trans_Amt']]
+    def _agrupar_marital_status(self):
+        return self.df_card.groupby('Marital_Status').mean()[['Total_Trans_Amt']]
 
     def _generate_grouped_plain_plot(self):
         plt.figure(figsize=(10, 5))
-        plot1 = plt.bar(x=self.df_groupedby.reset_index()['Marital_Status'],
-                height=self.df_groupedby.reset_index()['Total_Trans_Amt'])
+        plt.bar(x=self.df_groupedby.reset_index()['Marital_Status'],
+                        height=self.df_groupedby.reset_index()['Total_Trans_Amt'])
         plt.title(f'{self.card} card: Gasto agrupado por Total_Trans_Amt')
         plt.savefig(f'{self.card}_card_g1_SimplesGastoAgrupadoPorMaritalStatus.png')
         plt.clf()
 
-    def _generate_Churn_Education_plot(self):
+    def _generate_churn_education_plot(self):
         plt.figure(figsize=(10, 5))
-        plot2 = sns.countplot(x=self.df['Education_Level'], hue=self.df['Attrition_Flag'])
+        sns.countplot(x=self.df_card['Education_Level'], hue=self.df_card['Attrition_Flag'])
         plt.title(f'{self.card} card: Churn por Education Level')
         plt.savefig(f'{self.card}_card_g2_ChurnporEducationLevel.png')
         plt.clf()
 
     def _generate_corr_plot(self):
         plt.figure(figsize=(10, 8))
-        plot3 = sns.heatmap(self.df.corr(), cmap='coolwarm', vmin=-1, vmax=1)
+        sns.heatmap(self.df_card.corr(), cmap='coolwarm', vmin=-1, vmax=1)
         plt.title(f'{self.card} card: HeatMap de correlação')
         plt.savefig(f'{self.card}_card_g3_HeatMapDeCorrelacao.png')
         plt.clf()
-
 
     def _filter_by_card(self, card):
         dado_procurado = self.df['Card_Category'] == card
         df = self.df[dado_procurado]
         return df
+
 
 class Home(Resource):
     def get(self):
@@ -78,8 +79,9 @@ class Home(Resource):
                       "'Blue', 'Gold', 'Silver' ou 'Platinum'")
         return texto_home
 
+
 api.add_resource(Filtro,  '/filtro/<string:card>')
-api.add_resource(Home,'/')
+api.add_resource(Home, '/')
 
 
 if __name__ == '__main__':
